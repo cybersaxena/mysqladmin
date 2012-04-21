179b62548b5eb294892f7e8e4f7fa2e3ec13ad80
Oscar <sokerlp@gmail.com>
2012-04-20 19:47:29 -0500

    Se agrega  posible manejo de errores y se eliminan algunas impresiones de variables

--- action/listUser.php
+++ action/listUser.php
@@ -1,19 +1,14 @@
 <?php
         $db= new MySQLdBO();
-        echo $db->connect();
         $query = "SELECT  * FROM MYSQL.USER ";
         
         $result = $db->execQuery($query);
-        echo $result;
         $users = array();
         while($user=mysql_fetch_array($result)){
             $u = new User();
             $u->setUser ($user['User']);
             $u->setHost($user['Host']);
             $users[] = $u;
-        }
-        foreach($users as $us){
-            echo $us->getUser();
         }
         $view = 'listUser';
         $title = "Lista de Usuarios";
--- index.php
+++ index.php
@@ -1,12 +1,35 @@
 <?php
 include_once("db/mysql.php");
 include_once("classes/classes.php");
+//set_error_handler('handleError');
 if($action=$_GET['action']){
         $messagesOK = array();
         $messagesWarning = array();
         $messagesError = array();
         $messagesSQL = array();
+        try {
         include("action/$action.php");
+    } catch (Exception $e) {
+        $title = "Error";
+        $view = "listUser";
+    $messagesError[] =  'Excepci&oacute;n capturada: '.  $e->getMessage(). "\n";
+}       
         include("view/main.php");
 }
+
+
+function handleError($errno, $errstr, $errfile, $errline, array $errcontext)
+{
+    // error was suppressed with the @-operator
+    if (0 === error_reporting()) {
+        return false;
+    }
+
+    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+}
+
+
+
+
+
 ?>
