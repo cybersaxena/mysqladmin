877c5b723c77c518587a922741b1bba100b983ab
Oscar <sokerlp@gmail.com>
2012-04-21 07:13:58 -0500

    estilo

--- theme/footer.php
+++ theme/footer.php
@@ -1,1 +1,7 @@
 </div>
+<div id="footer">Creado por:<br>
+Oscar Casarrubias Paredes | Sergio Arturo Garc&iacutea; Mart&iacute;nez | David Ricardo Ruiz Reyes <br>
+<br>
+Diplomado de administraci—n de Bases de Datos - DGTIC
+
+</div>
